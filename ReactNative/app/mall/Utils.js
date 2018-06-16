5d1efd3b300ef805996d458b713fe4c45bb705cc
李江东 <jiangdongli@pptv.com>
2018-06-16 16:06:12 +0800

    commit

--- ReactNative/app/mall/Utils.js
+++
@@ -1,8 +0,0 @@
-
-export function navigatorGoBack(navigator) {
-    if (navigator && navigator.getCurrentRoutes().length>0){
-        navigator.pop();
-        return true;
-    }
-    return false;
-}
